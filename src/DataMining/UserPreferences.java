d0ad287ecb0b70b3801f27cd91afb45171e5aefb
Andra Veraart <averaart@hotmail.com>
2012-04-26 23:52:15 +0200

    Ratings kunnen nu aan een UserPref toegevoegd worden. Deze worden gesorteerd op ItemID opgeslagen.

--- src/DataMining/UserPreferences.java
+++ src/DataMining/UserPreferences.java
@@ -14,45 +14,71 @@ import com.mongodb.DBCursor;
 import com.mongodb.MongoException;
 
 /**
- * User: Maarten
- * Date: 25-4-12
- * Time: 11:46
+ * User: Maarten Date: 25-4-12 Time: 11:46
  */
 public class UserPreferences {
 
-    private int userId;
-    private int[] itemIds;
-    private double[] ratings;
+	private int userId;
+	private int[] itemIds;
+	private double[] ratings;
 
-    public UserPreferences(int id){
-        this.userId = id;
-        this.itemIds = new int[0];
-        this.ratings = new double[0];
-    }
+	public UserPreferences(int id) {
+		this.userId = id;
+		this.itemIds = new int[0];
+		this.ratings = new double[0];
+	}
 
-    public void addRatings(Map<Integer, Double> prefs)
-    {
-        int nextPos = itemIds.length;
-        int[] new_itemIds = Arrays.copyOf(itemIds, (nextPos+prefs.size()));
-        double[] new_ratings = Arrays.copyOf(ratings, (nextPos+prefs.size()));
+	public void addRatings(int newId, Double newRating) {
+			// zoek de eerstvolgende logische locatie X
+			int newIndex = Arrays.binarySearch(itemIds, newId);
+						
+			if (itemIds.length == 0){					// De eerste keer maken we gewoon een nieuwe array aan
+				itemIds = new int[]{newId};
+				ratings = new double[]{newRating};
+			} else if (newIndex >= 0){ 	// Als de itemId al bestaat slaan we alleen de nieuwe rating op
+				ratings[newIndex] = newRating;
+			} else {									// Anders wordt een nieuw item tussen gevoegd
+				
+				newIndex *= -1;
+				newIndex -= 1;
+								
+				// Kopie‘n met extra ruimte
+				int[] newItemIds = Arrays.copyOf(itemIds, itemIds.length+1);
+				double[] newRatings = Arrays.copyOf(ratings, ratings.length+1);
+				
+				if (newItemIds.length > newIndex+1){	// Opschuiven, tenzij de nieuwe op het einde moet
+					System.arraycopy(itemIds, newIndex, newItemIds, newIndex+1, newItemIds.length-newIndex-1);
+					System.arraycopy(ratings, newIndex, newRatings, newIndex+1, newRatings.length-newIndex-1);					
+				}
+				
+				// Beschikbaar gekomen ruimte vullen
+				newItemIds[newIndex] = newId;
+				newRatings[newIndex] = newRating;
+				
+				// nieuwe lijsten terugplaatsen
+				itemIds = newItemIds;
+				ratings = newRatings;
+			}
 
-        for (Map.Entry<Integer, Double> entry : prefs.entrySet())
-        {
-            new_itemIds[nextPos] = entry.getKey();
-            new_ratings[nextPos++] = entry.getValue();
-        }
+	}
+	
+	public String toString(){
+		String result = "";
+		
+		for (int i=0; i<itemIds.length; i++){
+			result += itemIds[i] +" - "+ ratings[i] +"\n";
+		}
+		result += "\n";
+		
+		return result;
+	}
 
-        itemIds = new_itemIds;
-        ratings = new_ratings;
-    }
-
-
-    /**
+	/**
      *
      */
-    public static void main(String[] args) {
-    	
-    	Mongo m = null;
+	public static void main(String[] args) {
+
+		Mongo m = null;
 		try {
 			m = new Mongo();
 		} catch (UnknownHostException e) {
@@ -63,36 +89,28 @@ public class UserPreferences {
 			e.printStackTrace();
 		}
 
-    	DB db = m.getDB( "mydb" );
-    	
-    		
-        UserPreferences uP = new UserPreferences(1);
-        HashMap<Integer, Double> prefs = new HashMap<Integer, Double>();
-        prefs.put(1, 2.0);
-        prefs.put(2, 3.0);
-        prefs.put(3, 9.0);
-        prefs.put(4, 7.0);
-        prefs.put(5, 1.0);
-        prefs.put(6, 6.0);
+		DB db = m.getDB("mydb");
 
+		UserPreferences uP = new UserPreferences(1);
 
-        uP.addRatings(prefs);
-        for (double rating : uP.ratings) {
-            System.out.println(rating);
-        }
+		uP.addRatings(3, 4.0);
+		System.out.print(uP.toString());
 
-        System.out.print("\n");
+		uP.addRatings(5, 3.5);
+		System.out.print(uP.toString());
 
-        uP.addRatings(prefs);
-        for (double rating : uP.ratings) {
-            System.out.println(rating);
-        }
+		uP.addRatings(2, 1.0);
+		System.out.print(uP.toString());
 
-        System.out.print("\n");
+		uP.addRatings(3, 3.5);
+		System.out.print(uP.toString());
 
-        for (int id : uP.itemIds) {
-            System.out.println(id);
-        }
-    }
+		uP.addRatings(1, 5.0);
+		System.out.print(uP.toString());
+
+		uP.addRatings(4, 5.0);
+		System.out.print(uP.toString());
+
+	}
 
 }
