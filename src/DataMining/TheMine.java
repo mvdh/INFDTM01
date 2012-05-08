befb354407d0bec0c38b171ac5ab4f600ac7cb70
Andra Veraart <averaart@hotmail.com>
2012-05-08 14:07:30 +0200

    Data can now be imported from a textfile into a collection. Data can also be pulled from a collection.

--- src/DataMining/TheMine.java
+++ src/DataMining/TheMine.java
@@ -33,36 +33,49 @@ public class TheMine {
 	 */
 	public static void main(String[] args) {
 		
-		// Get database connection and database instance
-		try {
-			con = new Mongo( "localhost" , 27017 );
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (MongoException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		db = con.getDB( "infdtm" );
+		connect();		
 		
-		
-		userPrefs = new TreeMap<Integer, UserPreferences>();
-
 		
 		/*
-		 * Old script to insert random ratings for random products by random users
+		 * Load and/or print test data
 		 */
-		// r = new Random(1138);
-		// for (int i = 0; i < 70; i++) {
-		// addRating(r.nextInt(7) + 1, r.nextInt(10) + 1,
-		// ((double) r.nextInt(11)) / 2);
-		// }
+		
+		userPrefs = new TreeMap<Integer, UserPreferences>();
+		
+		// Import the test data
+		if (db.getCollection("test_data").count() == 0)
+			loadFileIntoDatabase("testdata.txt", "test_data");
 
-		loadFile();
+		// Load data from database into userPrefs
+		loadFromDatabase("test_data");
 
+		// Print all ratings for all users
 		for (UserPreferences uP : userPrefs.values()) {
 			System.out.println(uP.toString());
 		}
+
+	
+		/*
+		 * Load and/or print movie data
+		 */
+		
+		userPrefs = new TreeMap<Integer, UserPreferences>();
+		
+		// Import the test data
+		if (db.getCollection("movie_ratings").count() == 0)
+			loadFileIntoDatabase("u.data", "movie_ratings");
+
+		// Load data from database into userPrefs
+		loadFromDatabase("movie_ratings");
+
+		// Print all ratings for all users
+		for (UserPreferences uP : userPrefs.values()) {
+			System.out.println(uP.toString());
+		}
+
+				
+		// Close connection to database
+		con.close();
 
 	}
 
@@ -74,6 +87,35 @@ public class TheMine {
 		uP.addRating(itemId, rating);
 	}
 
+	
+	/*
+	 * Loads the sample data from database and passes it directly into the collection of ratings
+	 */
+	public static void loadFromDatabase(String collection) {
+
+		DBObject doc;
+		DBCollection coll = db.getCollection(collection);
+
+		// Build a filter for fields to limit the amount of data sent back from DB
+		BasicDBObject subset = new BasicDBObject();
+		subset.put("user_id", 1);
+		subset.put("item_id", 1);
+		subset.put("rating", 1);
+		subset.put("_id", 0);
+		
+		// First argument is an empty document to gain access to the second argument
+		// The sort is applied in case a movie has been rated twice by the same user 
+		DBCursor cur = coll.find(new BasicDBObject(), subset).sort(new BasicDBObject("date", 1));
+		
+        while(cur.hasNext()) {
+        	doc = cur.next();
+        	int userId = (Integer)doc.get("user_id");
+        	int itemId = (Integer)doc.get("item_id");
+        	double rating = (Double)doc.get("rating");
+        	addRating(userId, itemId, rating);
+        }
+	}	
+	
 	/*
 	 * Loads the sample data file and passes it directly into the collection of ratings
 	 */
@@ -113,14 +155,14 @@ public class TheMine {
 	/*
 	 * Loads the sample data file and stores the data into a mongodb collection
 	 */
-	public static void loadFileIntoDatabase() {
+	public static void loadFileIntoDatabase(String filename, String collection) {
 
-		DBCollection coll = db.getCollection("movie_ratings");
+		DBCollection coll = db.getCollection(collection);
 		BasicDBObject doc;
 		
 		FileInputStream fstream;
 		try {
-			fstream = new FileInputStream("u.data");
+			fstream = new FileInputStream(filename);
 
 			// Get the object of DataInputStream
 			DataInputStream in = new DataInputStream(fstream);
@@ -135,8 +177,9 @@ public class TheMine {
 				doc.put("user_id", Integer.parseInt((String) st.nextElement()));
 				doc.put("item_id", Integer.parseInt((String) st.nextElement()));
 				doc.put("rating", Double.parseDouble((String) st.nextElement()));
-				doc.put("date", new Date(Long.parseLong((String) st.nextElement()) * 1000));
-				
+				if (st.hasMoreElements()){
+					doc.put("date", new Date(Long.parseLong((String) st.nextElement()) * 1000));
+				}
 				// Store new document
 				coll.insert(doc);
 			}
@@ -155,4 +198,32 @@ public class TheMine {
 
 	}
 	
+	/*
+	 * Old script to insert random ratings for random products by random users
+	 */
+	public static void addRandom(){
+		addRandom(1138);
+	}
+	
+	public static void addRandom(int seed){
+		r = new Random(seed);
+		for (int i = 0; i < 70; i++) {
+			addRating(r.nextInt(7) + 1, r.nextInt(10) + 1, ((double) r.nextInt(11)) / 2);
+		}		
+	}
+
+	public static void connect(){
+		// Get database connection and database instance
+		try {
+			con = new Mongo( "localhost" , 27017 );
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MongoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		db = con.getDB( "infdtm" );
+	}
+
 }
